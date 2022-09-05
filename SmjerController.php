7a4ffd4f56fff99813f3ebefca6413576fd2776a
Antonio Vuletić <104829640+antonio2212@users.noreply.github.com>
2022-09-05 12:18:25 +0200

    05_09_

--- SmjerController.php
+++ SmjerController.php
@@ -1,9 +1,21 @@
 <?php
 
-class SmjerController
+class SmjerController extends Controller 
 {
+
+    private $putanja = 'privatno' . 
+    DIRECTORY_SEPARATOR . 'smjer' . 
+    DIRECTORY_SEPARATOR; 
+
     public function index()
     {
-        echo 'Hello Smjer';
+        //echo 'Hello smjer';
+        //$view = new View();
+        $this->view->render($this->putanja . 'index',[
+            'grad'=>'Osijek',
+            'brojevi'=>[
+                1,4,7,8
+            ]
+            ]);
     }
 }
