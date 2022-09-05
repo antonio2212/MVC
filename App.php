b2524cfaab4609d212e1cf10b144465ec1cc33d0
Antonio Vuletić <104829640+antonio2212@users.noreply.github.com>
2022-09-05 12:19:49 +0200

    05_09_

--- App.php
+++ App.php
@@ -1,9 +1,38 @@
 <?php
 
-class App 
+class App
 {
     public static function start()
     {
-        echo 'Hello from App::start';
+        // echo 'Hello from App::start';
+        $ruta = Request::getRuta();
+
+        $dijelovi = explode('/', $ruta);
+
+        //Log::log($dijelovi);
+
+        $klasa='';
+        if(!isset($dijelovi[1]) || $dijelovi[1]===''){
+            $klasa = 'IndexController';
+        }else{
+            $klasa = ucfirst($dijelovi[1]) . 'Controller';
+        }
+        
+        //Log::log($klasa);
+
+        $metoda = '';
+        if(!isset($dijelovi[2]) || $dijelovi[2]===''){
+            $metoda = 'index';
+        }else{
+            $metoda = $dijelovi[2];
+        }
+         //Log::log($metoda);
+
+        if(class_exists($klasa) && method_exists($klasa, $metoda)){
+            $instanca = new $klasa();
+            $instanca->$metoda();
+        }else{
+            echo 'Ne postoji ' . $klasa . '-&gt' . $metoda;
+        }
     }
 }
